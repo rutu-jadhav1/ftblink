a275f1d094533d661b94fe2c3e4343dd236c17ba
rutu-jadhav1 <0103rutujajadhav@gmail.com>
2024-08-03 10:43:00 +0530

    router added

--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
 
+import './index.css';
+import Home from './views/Home/Home';
+
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <Home/>
+  }
+])
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
- <h1>Hiiiiiiii</h1>
-);
+root.render( <RouterProvider router={router}/>);
