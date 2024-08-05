9a02ea9471cd7d3c7d40dfdc715d82e6fff5d1ce
rutu-jadhav1 <0103rutujajadhav@gmail.com>
2024-08-05 23:08:41 +0530

    target updated in linkcard

--- client/src/components/LinkCard/LinkCard.js
+++ client/src/components/LinkCard/LinkCard.js
@@ -9,7 +9,9 @@ function LinkCard({ title, slug, target, views, createdAt }) {
                 <a href=''>{slug}</a>
             </p>
             <p className='link-card-target'>
-                <a href={target} target='_blank' className='link-card-target'>{target.substring(0, 50)}</a>
+                <a href={target} target='_blank' className='link-card-target'>
+                    {target.substring(0, 50)}{target.length > 50 ? "..." : null}
+                </a>
             </p>
             <span className='link-card-view'>{views}</span><br />
             <span className='link-card-date'>{new Date(createdAt).toLocaleString()}</span>
