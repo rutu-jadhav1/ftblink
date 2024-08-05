a9ead4fbdd263e24e498a753aed9a95445b1b8b2
rutu-jadhav1 <0103rutujajadhav@gmail.com>
2024-08-05 12:28:58 +0530

    target,view and date css added

--- client/src/components/LinkCard/LinkCard.js
+++ client/src/components/LinkCard/LinkCard.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import "./LinkCard.css"
 
-function LinkCard({title,slug,target,views,createdAt}) {
-  return (
-    <div>
-        {title}
-        {slug}
-        {target}
-        {views}
-        {createdAt}
-    </div>
-  )
+function LinkCard({ title, slug, target, views, createdAt }) {
+    return (
+        <div className='link-card-container'>
+            <h3 className='link-card-title'>{title}</h3>
+            <p className='link-card-target'>
+                <a href=''>{slug}</a>
+            </p>
+            <p className='link-card-target'>
+                <a href={target} target='_blank' className='link-card-target'>{target.substring(0, 50)}</a>
+            </p>
+            <span className='link-card-view'>{views}</span><br />
+            <span className='link-card-date'>{new Date(createdAt).toLocaleString()}</span>
+        </div>
+    )
 }
 
 export default LinkCard
