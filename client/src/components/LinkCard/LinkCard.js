049a1467d9d34ea9c232932b35db4fedffc43a12
rutu-jadhav1 <0103rutujajadhav@gmail.com>
2024-08-05 23:35:22 +0530

    view updated in linkcard

--- client/src/components/LinkCard/LinkCard.js
+++ client/src/components/LinkCard/LinkCard.js
@@ -1,19 +1,26 @@
 import React from 'react'
 import "./LinkCard.css"
+import shortImg from './short.png'
+import targetImg from './target.png'
 
 function LinkCard({ title, slug, target, views, createdAt }) {
+    const shortURL = `${process.env.REACT_APP_BACKEND_URL}/${slug}`
     return (
         <div className='link-card-container'>
             <h3 className='link-card-title'>{title}</h3>
-            <p className='link-card-target'>
-                <a href=''>{slug}</a>
-            </p>
-            <p className='link-card-target'>
-                <a href={target} target='_blank' className='link-card-target'>
-                    {target.substring(0, 50)}{target.length > 50 ? "..." : null}
+                <a href={shortURL} target='_blank' className='link-card-target'>
+                    <img src={shortImg} alt='ShortURL'className='link-card-img'/>{shortURL}
                 </a>
-            </p>
-            <span className='link-card-view'>{views}</span><br />
+                <a href={target} target='_blank' className='link-card-target'>
+                   <img src={targetImg} alt='TargetURL' className='link-card-img'/>{target.substring(0, 50)}{target.length > 50 ? "..." : null}
+                </a>
+         
+            <span className='link-card-view'>
+                {views}
+                </span>
+            <span className='link-card-view-text'>
+                {views > 0 ? `${views} people visited this URL` : 'Share URL to get view count'}
+                </span>
             <span className='link-card-date'>{new Date(createdAt).toLocaleString()}</span>
         </div>
     )
