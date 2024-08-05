85052c6a6c9e9fb61816464d6c499ae2d1edcb43
rutu-jadhav1 <0103rutujajadhav@gmail.com>
2024-08-05 15:34:28 +0530

    register page css updated

--- client/src/views/Register/Register.js
+++ client/src/views/Register/Register.js
@@ -7,31 +7,36 @@ import registerlogo from './register.png'
 function Register() {
     return (
         <div>
-            <form className='register-form'>
+            <form className='register-form text-style'>
                 <div className='register-name-container'>
-                <h2>Register Here</h2>
-                <img src={registerlogo} alt='register-img' className='register-img'/>
+                    <h2>Register Here</h2>
+                    <img src={registerlogo} alt='register-img' className='register-img' />
                 </div>
                 <div className="form-group">
                     <label for="exampleInputEmail1">Full Name</label>
-                    <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your Fullname" />
+                    <input type="text" className="form-control register-input" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Fullname" />
                 </div>
                 <div className="form-group">
                     <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                    <input type="email" className="form-control register-input" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
                     <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                 </div>
                 <div className="form-group">
                     <label for="exampleInputPassword1">Password</label>
-                    <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password" />
+                    <input type="password" className="form-control register-input" id="exampleInputPassword1" placeholder="Password" />
                 </div>
                 <div className="form-group">
                     <label for="exampleInputEmail1">Profession</label>
-                    <input type="text" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your Profession" />
+                    <select class="form-control register-input" id="exampleFormControlSelect1">
+                        <option>select Profession</option>
+                        <option>Business</option>
+                        <option>Student</option>
+                        <option>Other</option>
+                    </select>
                 </div>
                 <div className="form-group">
                     <label for="exampleInputEmail1">DOB </label>
-                    <input type="date" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your Fullname" />
+                    <input type="date" className="form-control register-input" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your Fullname" />
                 </div>
 
                 <button type="button" className="register-btn">Register</button>
