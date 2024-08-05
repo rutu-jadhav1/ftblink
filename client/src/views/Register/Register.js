b792c07966cc88cf5476030b77b579670f917680
rutu-jadhav1 <0103rutujajadhav@gmail.com>
2024-08-05 16:46:24 +0530

    register page connected to db

--- client/src/views/Register/Register.js
+++ client/src/views/Register/Register.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
 
 import './Register.css'
 import registerlogo from './register.png'
 import { Link } from 'react-router-dom'
+import axios from 'axios'
+import toast, { Toaster } from 'react-hot-toast'
 
 function Register() {
+    const [user, setUser] = useState({
+        fullName: '',
+        email: '',
+        password: '',
+        dob: '',
+        profession: ''
+    })
+
+    const register = async () => {
+        const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/signup`, {
+            fullName: user.fullName,
+            email: user.email,
+            password: user.password,
+            dob: user.dob,
+            profession: user.profession
+        })
+        if (response.data.success) {
+            toast.success(response.data.message)
+            setUser({
+                fullName: '',
+                email: '',
+                password: '',
+                dob: '',
+                profession: ''
+            })
+        }
+        else {
+            toast.error(response.data.message)
+        }
+
+    }
     return (
         <div>
             <form className='register-form text-style'>
@@ -15,20 +48,28 @@ function Register() {
                 </div>
                 <div className="form-group">
                     <label for="exampleInputEmail1">Full Name</label>
-                    <input type="text" className="form-control register-input" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Fullname" />
+                    <input type="text" className="form-control register-input" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter Fullname"
+                        value={user.fullName}
+                        onChange={(e) => { setUser({ ...user, fullName: e.target.value }) }} />
                 </div>
                 <div className="form-group">
                     <label for="exampleInputEmail1">Email address</label>
-                    <input type="email" className="form-control register-input" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
+                    <input type="email" className="form-control register-input" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"
+                        value={user.email}
+                        onChange={(e) => { setUser({ ...user, email: e.target.value }) }} />
                     <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
                 </div>
                 <div className="form-group">
                     <label for="exampleInputPassword1">Password</label>
-                    <input type="password" className="form-control register-input" id="exampleInputPassword1" placeholder="Password" />
+                    <input type="password" className="form-control register-input" id="exampleInputPassword1" placeholder="Password"
+                        value={user.password}
+                        onChange={(e) => { setUser({ ...user, password: e.target.value }) }} />
                 </div>
                 <div className="form-group">
                     <label for="exampleInputEmail1">Profession</label>
-                    <select class="form-control register-input" id="exampleFormControlSelect1">
+                    <select class="form-control register-input" id="exampleFormControlSelect1"
+                        value={user.profession}
+                        onChange={(e) => { setUser({ ...user, profession: e.target.value }) }}>
                         <option>select Profession</option>
                         <option>Business</option>
                         <option>Student</option>
@@ -37,11 +78,14 @@ function Register() {
                 </div>
                 <div className="form-group">
                     <label for="exampleInputEmail1">DOB </label>
-                    <input type="date" className="form-control register-input" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your Fullname" />
+                    <input type="date" className="form-control register-input" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter your Fullname"
+                        value={user.dob}
+                        onChange={(e) => { setUser({ ...user, dob: e.target.value }) }} />
                 </div>
 
-                <button type="button" className="register-btn">Register</button>
+                <button type="button" className="register-btn" onClick={register}>Register</button>
                 <Link to='/login' className='register-pg-link'>Already have account? <span className='pg-link-nm'>Login</span></Link>
+                <Toaster />
             </form>
         </div>
     )
