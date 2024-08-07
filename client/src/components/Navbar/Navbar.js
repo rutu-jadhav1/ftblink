885cfb2ad6bb38ed2053986e70fe8d23e4e1f000
rutu-jadhav1 <0103rutujajadhav@gmail.com>
2024-08-07 12:38:38 +0530

    navbar login & logout logic implemented

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -1,20 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../../../node_modules/bootstrap/dist/css/bootstrap.min.css'
-import toast,{Toaster} from 'react-hot-toast'
+import toast, { Toaster } from 'react-hot-toast'
 import './Navbar.css'
 
 function Navbar() {
-    const logoutbt = async ()=>{
+    const [user, setUser] = useState('')
+
+    const logoutbt = async () => {
         localStorage.clear()
         toast.success('Logged out Successfully')
-     setTimeout(()=>{
-        window.location.href = '/login'
-     }, 3000)
-        
+        setTimeout(() => {
+            window.location.href = '/login'
+        }, 3000)
     }
-  return (
-    <div>
-         <nav className="navbar navbar-expand-lg shadow p-3 mb-5 bg-white rounded">
+
+    useEffect(() => {
+        const currentUser = JSON.parse(localStorage.getItem('currentUser'))
+        setUser(currentUser)
+    }, [])
+
+
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg shadow p-3 mb-5 bg-white rounded" >
                 <a className="navbar-brand web-nm" href="#">ShortenMe</a>
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
@@ -24,24 +32,30 @@ function Navbar() {
                         <li className="nav-item">
                             <a className="nav-link web-direc" href="#">Home</a>
                         </li>
-                        <li className="nav-item">
-                            <a className="nav-link web-direc" href="/login">Login</a>
-                        </li>
-                        <li className="nav-item">
-                        <span className='nav-link logot web-direc' onClick={logoutbt}>
-                            Logout
-                        </span>
-                        </li>
+
+                        {
+                            user ?
+                                <li className="nav-item">
+                                    <span className='nav-link logot web-direc' onClick={logoutbt}>
+                                        Logout
+                                    </span>
+                                </li>
+                                :
+                                <li className="nav-item">
+                                    <a className="nav-link web-direc" href="/login" >Login</a>
+                                </li>
+                        }
+
                         <li className="nav-item">
                             <a className="nav-link web-direc" href="/contact">Contact</a>
                         </li>
-                       
+
                     </ul>
                 </div>
             </nav>
-        <Toaster/>
-    </div>
-  )
+            <Toaster />
+        </div>
+    )
 }
 
 export default Navbar
